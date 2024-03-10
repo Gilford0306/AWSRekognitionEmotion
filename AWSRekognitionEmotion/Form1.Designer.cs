4a6228746c4a06619ce8b5ba84665ea0b35a8d4f
Gilford0306 <gilford0306@gmail.com>
2024-03-10 13:28:18 +0200

    Add project files.

---
+++ AWSRekognitionEmotion/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿using System.Windows.Forms;
+
+namespace AWSRekognitionEmotion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(92, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(629, 471);
+            this.panel1.TabIndex = 1;
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(7, 31);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(110, 36);
+            this.materialFlatButton1.TabIndex = 0;
+            this.materialFlatButton1.Text = "Add from File";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            this.materialFlatButton1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Location = new System.Drawing.Point(517, 31);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(108, 36);
+            this.materialFlatButton2.TabIndex = 2;
+            this.materialFlatButton2.Text = "Add from Url";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            this.materialFlatButton2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(349, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.materialFlatButton1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.materialFlatButton2);
+            this.panel2.Location = new System.Drawing.Point(92, 540);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(629, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 646);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Emotion Rekognition";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ AWSRekognitionEmotion/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.Runtime;
+using MaterialSkin;
+using Microsoft.Extensions.Configuration;
+
+namespace AWSRekognitionEmotion
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        private IConfiguration configuration;
+        private string accessKey;
+        private string secretKey;
+
+        private readonly AmazonRekognitionClient _rekognitionClient;
+        private PictureBox pictureBox1 = new PictureBox();
+        private readonly MaterialSkinManager materialSkinManager;
+        bool flag = false;
+        private float panelRatioWidth = 0.8f; 
+        private float panelRatioHeight = 0.7f;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            accessKey = configuration["AWS:AccessKey"];
+            secretKey = configuration["AWS:SecretKey"];
+            BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+            _rekognitionClient = new AmazonRekognitionClient(credentials, Amazon.RegionEndpoint.EUWest1);
+            panel2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+
+        }
+
+        private async Task<DetectFacesResponse> DetectEmotionsAsync(string imagePath)
+        {
+            using (var imageStream = new MemoryStream(File.ReadAllBytes(imagePath)))
+            {
+                var request = new DetectFacesRequest
+                {
+                    Image = new Amazon.Rekognition.Model.Image { Bytes = imageStream },
+                    Attributes = new List <string> { "ALL" } 
+                };
+
+                return await _rekognitionClient.DetectFacesAsync(request);
+            }
+        }
+
+        private void DisplayEmotions(DetectFacesResponse response)
+        {
+            if (response.FaceDetails.Count > 0)
+            {
+                var emotions = response.FaceDetails.SelectMany(faceDetail => faceDetail.Emotions)
+                    .Where(emotion => emotion.Confidence > 1); 
+
+                if (emotions.Any())
+                {
+                    var emotionText = string.Join("\n ", emotions.Select(emotion => $"{emotion.Type} ({emotion.Confidence:F0}%)"));
+                    MessageBox.Show($"Emotion Detectedи:\n{emotionText}", "Emotion Detected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Emotions not detected with confidence over 0%.", "Emotion Detected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("No face on Photo", "Emotion Detected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private async void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            try
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Image files ( *.png)|*.png";
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string filePath = openFileDialog.FileName;
+                        panel1.Controls.Remove(pictureBox1);
+                        pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                        pictureBox1.Image = System.Drawing.Image.FromFile(filePath);
+                        pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+                        panel1.Controls.Add(pictureBox1);
+                        pictureBox1.Image = System.Drawing.Image.FromFile(filePath);
+
+                        var detectEmotionsResponse = await DetectEmotionsAsync(filePath);
+
+                        DisplayEmotions(detectEmotionsResponse);
+
+                        if (pictureBox1.Image != null && detectEmotionsResponse.FaceDetails.Count > 0)
+                        {
+                            var graphics = Graphics.FromImage(pictureBox1.Image);
+                            var pen = new Pen(Color.Red, 3);
+
+                            foreach (var faceDetail in detectEmotionsResponse.FaceDetails)
+                            {
+                                var boundingBox = faceDetail.BoundingBox;
+                                var rect = new Rectangle(
+                                    (int)(boundingBox.Left * pictureBox1.Width),
+                                    (int)(boundingBox.Top * pictureBox1.Height),
+                                    (int)(boundingBox.Width * pictureBox1.Width),
+                                    (int)(boundingBox.Height * pictureBox1.Height));
+                                graphics.DrawRectangle(pen, rect);
+                            }
+
+                            pictureBox1.Refresh();
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void materialFlatButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                flag = false;
+                string imageUrl = textBox1.Text.Trim();
+                if (!string.IsNullOrEmpty(imageUrl))
+                {
+                    panel1.Controls.Remove(pictureBox1);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pictureBox1.Image = System.Drawing.Image.FromFile(await DownloadImageAsync(imageUrl)); ;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+                    panel1.Controls.Add(pictureBox1);
+                    DownloadImageAsync(imageUrl);
+                    var detectEmotionsResponse = await DetectEmotionsAsync(await DownloadImageAsync(imageUrl));
+                    DisplayEmotions(detectEmotionsResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private async Task<string> DownloadImageAsync(string url)
+        {
+            using (var client = new WebClient())
+            {
+                var data = await client.DownloadDataTaskAsync(url);
+                string fileName = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".png");
+                File.WriteAllBytes(fileName, data);
+                return fileName;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            SetPanelSize();
+            panel1.Refresh();
+        }
+        private void SetPanelSize()
+        {
+            int newWidth = (int)(this.ClientSize.Width * panelRatioWidth);
+            int newHeight = (int)(this.ClientSize.Height * panelRatioHeight);
+            panel1.Size = new Size(newWidth, newHeight);
+            panel2.Size = new Size(newWidth, this.panel2.Height);
+        }
+
+
+    }
+}
